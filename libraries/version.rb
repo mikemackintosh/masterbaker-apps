258c744153cc023fa349956bd348869e01b99af2
Mike Mackintosh <m@zyp.io>
2014-10-24 09:59:30 -0400

    fixed missing end keyword

--- libraries/version.rb
+++ libraries/version.rb
@@ -6,3 +6,8 @@ class Chef::Recipe
   def version_string_for(application_name)
     node['apps'][application_name]['version'] || raise("you must define a version for #{application_name} in attributes.")
   end
+
+  def checksum_string_for(application_name)
+    node['apps'][application_name]['checksum'] || raise("you must define a checksum for #{application_name} in attributes.")
+  end
+end
