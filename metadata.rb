219cc603102f477994fb00892b4892b5137307fd
Mike Mackintosh <m@zyp.io>
2014-10-23 20:28:00 -0400

    v0.2.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "m@splug.io"
 license          "Apache 2.0"
 description      "Manage Apps for OSX"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.2.1"
 supports         "apps"
 
 depends "homebrew"
