a59ba5e332d8c78a8a92a6f4aa5c1acc0f59a2ea
Mike Mackintosh <m@zyp.io>
2014-10-24 11:28:22 -0400

    added hopper

---
+++ attributes/hopper.rb
@@ -0,0 +1,2 @@
+default['apps']['hopper']['version'] = "3.5.9"
+default['apps']['hopper']['checksum'] = "45ff1e5304b2855736af4dbc7974408b77bd63ce"
---
+++ recipes/hopper.rb
@@ -0,0 +1,11 @@
+3.5.9.zip
+remote_file "#{Chef::Config[:file_cache_path]}/Hopper-#{node['apps']['hopper']['version']}.zip" do
+  source "http://www.hopperapp.com/HopperWeb/downloads/Hopper-#{node['apps']['hopper']['version']}.zip"
+  checksum node['apps']['hopper']['checksum']
+  notifies :run, "execute[unzip-hopper]", :immediately
+end
+
+execute "unzip-hopper" do
+  command "unzip -o #{Chef::Config[:file_cache_path]}/Hopper-#{}{node['apps']['hopper']['version']}.zip -d /Applications"
+  action :nothing
+end
