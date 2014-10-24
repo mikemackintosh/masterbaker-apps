ac9bb835f9621219b4bfe15e520b70b40b5c6d4b
Mike Mackintosh <m@zyp.io>
2014-10-24 14:34:25 -0400

    fixed issue with hopper

--- recipes/hopper.rb
+++ recipes/hopper.rb
@@ -1,11 +1,12 @@
-3.5.9.zip
-remote_file "#{Chef::Config[:file_cache_path]}/Hopper-#{node['apps']['hopper']['version']}.zip" do
-  source "http://www.hopperapp.com/HopperWeb/downloads/Hopper-#{node['apps']['hopper']['version']}.zip"
-  checksum node['apps']['hopper']['checksum']
-  notifies :run, "execute[unzip-hopper]", :immediately
-end
+unless ::File.directory?("/Applications/Hopper.app")
+	remote_file "#{Chef::Config[:file_cache_path]}/Hopper-#{node['apps']['hopper']['version']}.zip" do
+	  source "http://www.hopperapp.com/HopperWeb/downloads/Hopper-#{node['apps']['hopper']['version']}.zip"
+	  checksum node['apps']['hopper']['checksum']
+	  notifies :run, "execute[unzip-hopper]", :immediately
+	end
 
-execute "unzip-hopper" do
-  command "unzip -o #{Chef::Config[:file_cache_path]}/Hopper-#{}{node['apps']['hopper']['version']}.zip -d /Applications"
-  action :nothing
+	execute "unzip-hopper" do
+	  command "unzip -o #{Chef::Config[:file_cache_path]}/Hopper-#{}{node['apps']['hopper']['version']}.zip -d /Applications"
+	  action :nothing
+	end
 end
