023012e4f1314951377cdfdeb5e27f393b352578
Mike Mackintosh <m@zyp.io>
2014-10-27 13:20:32 -0400

    added Mou

--- metadata.rb
+++ metadata.rb
@@ -4,13 +4,17 @@ maintainer_email "m@splug.io"
 license          "Apache 2.0"
 description      "Manage Apps for OSX"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.4"
+version          "0.2.5"
 supports         "apps"
 
 depends "homebrew"
 depends "mac_os_x"
 depends "dmg"
 
+
+# 0.2.5
+# - Added Mou
+#
 # 0.2.4
 # - Added kaleidoscope
 #
