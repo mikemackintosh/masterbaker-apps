6c44fecf2107bc9adba898e2ca7a299c0c21d5eb
Mike Mackintosh <mikemackintosh@users.noreply.github.com>
2014-12-10 14:28:38 -0500

    Update chrome.rb

--- recipes/chrome.rb
+++ recipes/chrome.rb
@@ -8,7 +8,7 @@ unless ::File.directory?("/Applications/Google Chrome.app")
 	dmg_package 'Google Chrome' do
 	  dmg_name 'googlechrome'
 	  source   'https://dl-ssl.google.com/chrome/mac/stable/GGRM/googlechrome.dmg'
-	  checksum '07c5cc'
+	  checksum '07c5ccddc5122825130d5c4611bfd7a921be1f7baeac987d078736eef1566153'
 	  action   :install
 	end
 end
