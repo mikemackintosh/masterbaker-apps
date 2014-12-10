ee3cfaeb2d22eac7d3072891ed82f7759e82dcdf
Mike Mackintosh <mikemackintosh@users.noreply.github.com>
2014-12-10 14:50:08 -0500

    Update coda2.rb

--- attributes/coda2.rb
+++ attributes/coda2.rb
@@ -1,2 +1,2 @@
 default['apps']['coda2']['version'] = "2.5"
-default['apps']['coda2']['checksum'] = "764a0cf3b1aafa9619f287e57e0234ee4441e332"
+default['apps']['coda2']['checksum'] = "9393ef340e04b8dc201d735a3885d14f13e82a6544b47c1ff21c4ad6eacd32ad"
