7fa119dbc55e38d8594473de05b73e847917961c
Mike Mackintosh <mikemackintosh@users.noreply.github.com>
2014-12-10 14:18:15 -0500

    Update hopper.rb

--- attributes/hopper.rb
+++ attributes/hopper.rb
@@ -1,2 +1,2 @@
 default['apps']['hopper']['version'] = "3.5.9"
-default['apps']['hopper']['checksum'] = "45ff1e5304b2855736af4dbc7974408b77bd63ce"
+default['apps']['hopper']['checksum'] = "2df08e871b6bcff38326d5883d2e305a0543218320292dffed32d96cb0e6e454"
