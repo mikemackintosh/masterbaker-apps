2ed7b402b02fa508ebea003c4c7124ed46959fad
Mike Mackintosh <mikemackintosh@users.noreply.github.com>
2014-12-10 14:43:57 -0500

    Update sublime_text3.rb

--- attributes/sublime_text3.rb
+++ attributes/sublime_text3.rb
@@ -1,2 +1,2 @@
 default['apps']['sublime_text3']['version'] = "3065"
-default['apps']['sublime_text3']['checksum'] = "55d4a09d827270a3b8c5ac45433d8f41415b448d"
+default['apps']['sublime_text3']['checksum'] = "373dbfcddab7fb288351763402361cd4045f4a9510eb32f81938d0c156d161c3"
