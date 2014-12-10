c287b58315c57d410cf5f42c166dd4f2a6b3a721
Mike Mackintosh <mikemackintosh@users.noreply.github.com>
2014-12-10 14:36:16 -0500

    Update viscosity.rb

--- attributes/viscosity.rb
+++ attributes/viscosity.rb
@@ -1,2 +1,2 @@
 default['apps']['viscosity']['version'] = "1.5.2"
-default['apps']['viscosity']['checksum'] = "3944609ac296310d006425386113b6838d462220"
+default['apps']['viscosity']['checksum'] = "7d49941519401a37b38dc629b8b666c0e9654d86d4af0da1757a1aba3db83b14"
