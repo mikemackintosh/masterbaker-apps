b0a3d7bde3425bde9ab253e8d7485a3e06320f03
Mike Mackintosh <mikemackintosh@users.noreply.github.com>
2014-12-10 14:25:29 -0500

    Update alfred.rb

--- attributes/alfred.rb
+++ attributes/alfred.rb
@@ -1,2 +1,2 @@
 default['apps']['alfred']['version'] = "2.5_299"
-default['apps']['alfred']['checksum'] = "8d0f75e7738c5fd8fd5ff78daa14ae5ba9e7ede2"
+default['apps']['alfred']['checksum'] = "4654da77e195342285b57253d1177f94e68adb8be8d7b10bd05d26353f75ecf6"
