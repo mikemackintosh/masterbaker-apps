14c3d7560177a0452d2a539b5e0c722a497e15ba
Mike Mackintosh <mikemackintosh@users.noreply.github.com>
2014-12-10 15:03:46 -0500

    Update knox.rb

--- attributes/knox.rb
+++ attributes/knox.rb
@@ -1,2 +1,2 @@
 default['apps']['knox']['version'] = "2.2.0"
-default['apps']['knox']['checksum'] = "227e8f3fb2279ca1199e72a961d0f54c8ca8a479"
+default['apps']['knox']['checksum'] = "c19c56a35d299a2cd85c612e1e99009bff9d7536ddd24f9d565910702be9742a"
