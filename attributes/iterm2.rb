6a4c682234236cfa25e23fa8b480d0816c1663b0
Mike Mackintosh <mikemackintosh@users.noreply.github.com>
2014-12-10 14:05:22 -0500

    iterm2 update checksum

--- attributes/iterm2.rb
+++ attributes/iterm2.rb
@@ -1,2 +1,2 @@
 default['apps']['iterm2']['version'] = "v2_0"
-default['apps']['iterm2']['checksum'] = "9fc3db9ee2ceb4400ed76e01b6da8871fef5cfa1"
+default['apps']['iterm2']['checksum'] = "6a59e1b96d9037b6ff1b8aaaf7aed5cff572d74d6e58390db16028433769c058"
