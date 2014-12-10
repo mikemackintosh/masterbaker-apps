f9b996233b7307d05ac081db45d92d359129d32c
Mike Mackintosh <mikemackintosh@users.noreply.github.com>
2014-12-10 14:46:40 -0500

    Update slack.rb

--- attributes/slack.rb
+++ attributes/slack.rb
@@ -1,2 +1,2 @@
 default['apps']['slack']['url'] = "https://dl0tgz6ee3upo.cloudfront.net/production/app/builds/002/379/771/original/7287a01afa8921c37182c1d3e468edd3/Slack.app.zip"
-default['apps']['slack']['checksum'] = "71d422d8c0a10252753dd5a2b995a5e2a80e1397"
+default['apps']['slack']['checksum'] = "205af28a2785a2c4e6ca2b2cd936ec38f686f5e1c09ae00547fe48bab5b3ff79"
